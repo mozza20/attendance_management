c7d497a10e4e624cb6b1a9b23b05c7157c739191
Ayana Tani <chan.a.etude@gmail.com>
2025-07-10 22:32:50 +0900

    勤怠一覧CSS

--- src/resources/views/user/attendanceList.blade.php
+++ src/resources/views/user/attendanceList.blade.php
@@ -1,17 +1,36 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/attendanceList.css') }}">
+@endsection
+
 @section('content')
 <div class="content">
-    <h1 class="title">勤怠一覧</h1>
-    <div class="month">
-
-    </div>
-    <table class="">
-    <div class="">
+    <h1 class="title">勤怠一覧</h1>    
+    <div class="select-month">
        <a class="previous-month" href="">前月</a>
-       <p class="">2025/07</p>
+       <p class="current-month">2025/07</p>
        <a class="next-month" href="">翌月</a>
     </div>
+    <table class="attendance--table">
+        <tr class="">
+            <td>日付</td>
+            <td>出勤</td>
+            <td>退勤</td>
+            <td>休憩</td>
+            <td>合計</td>
+            <td>詳細</td>
+        </tr>
+        <tr class="">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+                <a class="" href="">詳細</a>
+            </td>
+
+        </tr>
     </table>
 </div>
 @endsection
