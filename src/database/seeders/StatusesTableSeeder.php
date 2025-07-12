f403e7631da83eed9096ff38851b769ac8880dbd
Ayana Tani <chan.a.etude@gmail.com>
2025-07-12 12:35:44 +0900

    users,admins,statusesテーブル、フォームリクエスト作成

---
+++ src/database/seeders/StatusesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $statuses=[
+            "勤務外",
+            "出勤中",
+            "休憩中",
+            "退勤済",
+            "承認待ち",
+            "承認済",
+        ];
+
+        foreach ($statuses as $status) {
+            DB::table('statuses')->insert([
+                'status' => $status,
+            ]);
+        }
+    }
+}
